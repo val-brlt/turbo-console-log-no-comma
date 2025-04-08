633d631660d66a49109aa8be58072222d2a22e0f
valentin.brault <valentin.brault@scalian.com>
2025-04-08 13:09:55 +0200

    Adaptation des tests + run sur plusieurs OS

--- src/test/runTests.ts
+++ src/test/runTests.ts
@@ -1,4 +1,5 @@
 import * as path from 'path';
+import * as os from 'os';
 import { runTests } from 'vscode-test';
 
 async function main() {
@@ -6,8 +7,22 @@ async function main() {
     const extensionDevelopmentPath = path.resolve(__dirname, '../../');
     const extensionTestsPath = path.resolve(__dirname, './testsRunner');
     
-    const vscodeVersion = 'stable';  
-    const platform = 'darwin-arm64';
+    const vscodeVersion = 'stable';
+
+    // Détection dynamique de la plateforme
+    let platform;
+    const type = os.type();
+    const arch = os.arch();
+
+    if (type === 'Darwin') {
+      platform = arch === 'arm64' ? 'darwin-arm64' : 'darwin';
+    } else if (type === 'Windows_NT') {
+      platform = 'win32-x64-archive';
+    } else if (type === 'Linux') {
+      platform = arch === 'arm64' ? 'linux-arm64' : 'linux-x64';
+    } else {
+      throw new Error(`Unsupported OS: ${type}`);
+    }
 
     await runTests({ 
       version: vscodeVersion, 
